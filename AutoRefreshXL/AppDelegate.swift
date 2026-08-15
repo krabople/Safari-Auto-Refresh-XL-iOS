a6bb0b5fc2aee56887eb81b111efacda1d8f6a9f
krabople <166812596+krabople@users.noreply.github.com>
2026-08-15 22:28:28 +0100

    Add programmatic SceneDelegate and updated AppDelegate to resolve scene lifecycle crash

--- AutoRefreshXL/AppDelegate.swift
+++ AutoRefreshXL/AppDelegate.swift
@@ -6,13 +6,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+            // SceneDelegate will handle window creation on iOS 13+
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
+    // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let config = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 }
